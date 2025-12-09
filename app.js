16d460fb9ce94607fdf9e6c2474cf43fa0dc0b35
rafaelygomesifpr <rafaely.gomes.ifpr@gmail.com>
2025-12-08 22:47:10 -0300

    Controle de finanças

---
+++ app.js
@@ -0,0 +1,69 @@
+let salario = Number(localStorage.getItem("salario")) || 0;
+let despesas = JSON.parse(localStorage.getItem("despesas")) || [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    atualizarTela();
+});
+
+function salvarSalario() {
+    let valor = document.getElementById("salario").value;
+
+    if (valor === "") {
+        alert("Digite um salário válido!");
+        return;
+    }
+
+    salario = Number(valor);
+    localStorage.setItem("salario", salario);
+    atualizarTela();
+}
+
+function adicionarDespesa() {
+    let data = document.getElementById("data").value;
+    let nome = document.getElementById("nome").value;
+    let valor = document.getElementById("valor").value;
+
+    if (data === "" || nome === "" || valor === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    despesas.push({ data, nome, valor: Number(valor) });
+
+    localStorage.setItem("despesas", JSON.stringify(despesas));
+
+    atualizarTela();
+}
+
+function excluirDespesa(indice) {
+    despesas.splice(indice, 1);
+    localStorage.setItem("despesas", JSON.stringify(despesas));
+    atualizarTela();
+}
+
+function atualizarTela() {
+    document.getElementById("ressalario").innerText = salario;
+
+    let totalDespesas = despesas.reduce((soma, d) => soma + d.valor, 0);
+    document.getElementById("restotal").innerText = totalDespesas;
+
+    document.getElementById("ressaldo").innerText = salario - totalDespesas;
+
+    let tabela = document.getElementById("tabeladespesas");
+    tabela.innerHTML = "";
+
+    despesas.forEach((d, i) => {
+        tabela.innerHTML += `
+            <tr>
+                <td>${d.data}</td>
+                <td>${d.nome}</td>
+                <td>R$ ${d.valor}</td>
+                <td><button onclick="excluirDespesa(${i})">Excluir</button></td>
+            </tr>
+        `;
+    });
+}
+
+
+
+
